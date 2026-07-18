abf01bc3528a1516173c05f0ea1923140dddb68d
mciiee <mrchingiz91@gmail.com>
2026-07-19 00:20:48 +0500

    init: initial commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module math-uni-bot/v2
+
+go 1.26.5
+
+require (
+	github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+)
---
+++ src/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	godotenv "github.com/joho/godotenv"
+)
+
+const telegramTokenEnvVar = "TELEGRAM_BOT_TOKEN"
+
+func getToken() string {
+	err := godotenv.Load()
+	
+	if err != nil {
+    log.Print("Failed to load .env, falling back to checking the environment variables")
+	}
+  
+	token, present := os.LookupEnv(telegramTokenEnvVar)
+
+	if !present {
+		log.Printf("\"%s\" is not present in the environment", telegramTokenEnvVar)
+    os.Exit(1)
+  }
+
+	return token
+}
+
+func main() {
+
+	token := getToken()
+
+  bot, err := tgbotapi.NewBotAPI(token)
+
+	if err != nil {
+    log.Panic("Failed to init the bot: ", err)
+	}
+
+  bot.Debug = true
+
+	log.Printf("Authorized on account \"%s\"", bot.Self.UserName)
+
+	updateConf := tgbotapi.NewUpdate(0)
+	updateConf.Timeout = 60
+  
+	updates := bot.GetUpdatesChan(updateConf)
+
+	for update := range updates {
+    if update.Message == nil {
+      continue
+		}
+    
+		log.Printf("[Message:%d] %s<%d>: \"%s\"\n", update.Message.Chat.ID, update.Message.From.UserName,  update.Message.From.ID, update.Message.Text)
+
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+		msg.ReplyToMessageID = update.Message.MessageID
+		bot.Send(msg)
+	}
+}
